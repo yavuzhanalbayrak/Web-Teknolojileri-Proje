35d1a1f976d494fe92ec71fe70032be31d6053ac
Yavuzhan ALBAYRAK <82886846+yavuzhanalbayrak@users.noreply.github.com>
2021-05-17 18:53:04 +0300

    Dosya 7
    5. ve 6. dosyaların hatalı yüklenmesi sonucunda yüklendi. İletişim kısmının arayüzü tamamlandı. Şehrim kısmındaki resimlere ait açıklama linkleri eklendi. Sayfa arayüzünde küçük değişiklikler yapıldı.

---
+++ bilgi.php
@@ -0,0 +1,22 @@
+
+<table>
+    <tr>
+        <td>Kullanıcı adı</td><td>:</td>
+        <td><b>
+        <?php
+             echo $_POST['email']
+        ?>
+        <b>
+        </td>
+    </tr>
+      <tr>
+        <td>Şifre</td><td>:</td>
+        <td><b>
+        <?php
+             echo $_POST['password']
+        ?>
+        <b>
+        </td>
+    </tr>
+</table>
+<a style=" color:black; text-decoration:none; border-style:solid; border-width:0.5px; " href="anasayfa.html">Geri Dön</a>
