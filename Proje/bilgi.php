f0b7d6952929b7a548e7709ff3e8f71720910ad4
Yavuzhan ALBAYRAK <82886846+yavuzhanalbayrak@users.noreply.github.com>
2021-05-17 18:32:12 +0300

    dosya 5
    Sayfanın genel düzeni ile ilgili değişiklikler yapıldı. Footer eklendi. Şehrim kısmındaki resimlere açıklama linkleri eklendi. İletişim kısmının arayüzü bitti.

--- Proje/bilgi.php
+++ Proje/bilgi.php
@@ -1,11 +1,22 @@
 
 <table>
-<tr>
-<td>ADI SOYADI</td><td>:</td>
-<td><b>
-<?php
-echo $_POST['email']."&nbsp;".$_POST['password'];
-?>
-<b></td>
-</tr>
+    <tr>
+        <td>Kullanıcı adı</td><td>:</td>
+        <td><b>
+        <?php
+             echo $_POST['email']
+        ?>
+        <b>
+        </td>
+    </tr>
+      <tr>
+        <td>Şifre</td><td>:</td>
+        <td><b>
+        <?php
+             echo $_POST['password']
+        ?>
+        <b>
+        </td>
+    </tr>
 </table>
+<a style=" color:black; text-decoration:none; border-style:solid; border-width:0.5px; " href="anasayfa.html">Geri Dön</a>
